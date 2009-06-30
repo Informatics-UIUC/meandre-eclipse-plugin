7e3d09076a143b6b8fc0cad36f410190eb3bd3a9
Amit Kumar <amitku@illinois.edu>
2009-06-30 16:48:28 +0000

    added tag shell script

---
+++ tag.sh
@@ -0,0 +1,2 @@
+TAG=1.3.0
+svn copy svn://nora.lis.uiuc.edu/org.meandre.ide.eclipse.component/trunk svn://nora.lis.uiuc.edu/org.meandre.ide.eclipse.component/tags/$TAG -m "Tagging the $TAG release of the plugin"
